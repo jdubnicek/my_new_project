6b399c63de30ca7d37455a3ba29f0a6de8cfdbfc
Dubnicek, Jack <Jack.Dubnicek@ul.com>
2017-07-11 21:08:40 -0500

    Build out controller for menu items

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,19 +1,47 @@
 class MenuItemsController < ApplicationController
   def index
+    @menu_items = MenuItem.all
   end
 
   def new
+    @menu_item = MenuItem.new
   end
 
   def create
+    @menu_item = MenuItem.new(params)
+    if @user.save
+      redirect_to menu_items_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @menu_item = MenuItem.find_by_id(params[:id])
   end
 
   def update
+    @menu_item = MenuItem.find_by_id(params[:id])
+    if @menu_item.update
+      redirect_to menu_item_path
+    else
+      render :index******
+    end
   end
 
   def destroy
+    @menu_item = MenuItem.find_by_id(params[:id])
+    if @menu_item.delete
+      redirect_to menu_items_path
+    else
+      render :index****
+    end
+  end
+
+
+  private
+
+  def menu_item_params
+    params.require(:user).permit(:name, :description, :price, :category_id)
   end
 end
