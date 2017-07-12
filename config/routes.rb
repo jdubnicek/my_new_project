b0bb8f6706572e4f7350262f1220286b70baff4b
Dubnicek, Jack <Jack.Dubnicek@ul.com>
2017-07-11 22:55:00 -0500

    Add some gems, create basic user view and root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'user#index'
   resources :users
   resources :menu_items
 
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CartControllerTest < ActionDispatch::IntegrationTest
+class CartsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get cart_new_url
     assert_response :success
