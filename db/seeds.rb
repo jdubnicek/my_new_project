0b8259ef56b2066ac30829fa11d643f91ff6b334
Dubnicek, Jack <Jack.Dubnicek@ul.com>
2017-07-13 17:47:04 -0500

    Begin creating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating Users"
+users = [["Jack Dubnicek", "jdubnicek@gmail.com"],
+              ["Noel Rappin", "noel@tablexi.com"],
+              ["Table XI User", "contact@tablexi.com"]]
+
+cart_id = Cart.create!(user_id)
+
+users.each do |user|
+  User.create!({name: admin[0],
+                email: admin[1],
+                cart_id: " "})
+  puts "User #{user[0]} #{user[1]} has been created"
+end
+
+
+puts "Creating Categories"
+categories = []
+
+categories.each do |category|
+
+end
+
+
+puts "Creating Menu Items"
+items = [[],[],[],[]]
+
+items.each do |item|
+
+end
